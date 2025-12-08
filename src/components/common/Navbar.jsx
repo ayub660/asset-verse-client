948aa6e76f452abab814bf59ff0965f3eb0647b0
Ayub <md.ayub0070@gmail.com>
2025-12-08 23:54:24 +0600

    footer added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,13 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm p-4">
-            <a className="btn btn-ghost text-xl">AssetVerse</a>
-            <div className="flex-1"></div>
-            <div className="flex gap-4">
-                <a className="btn btn-ghost">Login</a>
-                <a className="btn btn-primary">Register</a>
+        <div className="navbar bg-base-100 shadow-md px-6">
+            <div className="navbar-start">
+                <Link to="/" className="btn btn-ghost normal-case text-xl">
+                    AssetVerse
+                </Link>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register-hr">Register HR</Link>
+                        </li>
+                        <li>
+                            <Link to="/register-employee">Register Employee</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register-hr">Register HR</Link>
+                    </li>
+                    <li>
+                        <Link to="/register-employee">Register Employee</Link>
+                    </li>
+                </ul>
             </div>
         </div>
     );
