4d97e11093ccfaff9709047bbf273837784e3d2f
Ayub <md.ayub0070@gmail.com>
2025-12-08 23:06:29 +0600

    install daisy

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,20 +1,9 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../common/Navbar";
-import Footer from "../common/Footer";
+import React from 'react';
 
 const MainLayout = () => {
     return (
-        <div className="flex flex-col min-h-screen">
-            {/* Navbar */}
-            <Navbar />
+        <div>
 
-            {/* Main content */}
-            <div className="flex-1">
-                <Outlet />
-            </div>
-
-            {/* Footer */}
-            <Footer />
         </div>
     );
 };
